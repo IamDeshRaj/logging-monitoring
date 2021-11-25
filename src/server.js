cbc334c38dd4496d5582b7ac2861c3e70345cc7e
DESH RAJ <deshu44@gmail.com>
2021-11-25 15:36:08 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Logger loading must be the fist statement
 const logger = require("./config/logging-client");
 
-
+// Configure your server first, logger can be used to log the errors, info etc
 server
   .listen({
     port: ,
